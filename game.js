5d78733d2021bfc9c3dea7976210eff4bad4a152
Merwann_tech <merwannheret@gmail.com>
2025-06-23 14:17:10 +0200

    js

--- game.js
+++ game.js
@@ -1,2 +1,7 @@
 import { quiz_Frida } from './questions.js'
 
+const questions = document.getElementsByClassName("question")
+const question1 = quiz_Frida.questions[0].text
+
+
+console.log(question1)
