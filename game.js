dbd0eeb7ca0080cbbb0164a94c31d8f2ee371d3c
Merwann_tech <merwannheret@gmail.com>
2025-06-23 15:56:07 +0200

    js update

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 import { quiz_Frida } from './questions.js'
 
-const questions = document.getElementsByClassName("question")
+const AfficherQuestions = document.querySelector(".question")
+const AfficherOption = document.querySelector(".options")
 const question1 = quiz_Frida.questions[0].text
 
+AfficherQuestions.innerText = question1
 
-console.log(question1)
+quiz_Frida.questions[0].options.forEach(elem => {
+    const option_btn = document.createElement('button');
+    option_btn.innerText = elem;
+    option_btn.classList.add('answer');
+    AfficherOption.appendChild(option_btn);
+});
