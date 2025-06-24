37e77c58102dcac481e176f338dbfe268896f809
Merwann_tech <merwannheret@gmail.com>
2025-06-24 10:38:28 +0200

    js update

--- game.js
+++ game.js
@@ -5,16 +5,60 @@ import { quiz_Frida } from './questions.js'
 const AfficherQuestions = document.querySelector(".question")
 // Sélectionne l'élément HTML où les options seront affichées
 const AfficherOption = document.querySelector(".options")
-// Récupère le texte de la première question du quiz
-const question1 = quiz_Frida.questions[0].text
 
-// Affiche la première question dans l'élément sélectionné
-AfficherQuestions.innerText = question1
+// Sélectionne le bouton "Suivant" par son identifiant
+const suivant = document.querySelector("#next-button")
+//const suivant = document.getElementByClass("next-button")
+//const suivant = document.getElementById("next-button")
 
-// Pour chaque option de la première question, crée un bouton et l'ajoute à la page
-quiz_Frida.questions[0].options.forEach(elem => {
-    const option_btn = document.createElement('button'); // Crée un bouton
-    option_btn.innerText = elem; // Définit le texte du bouton
-    option_btn.classList.add('answer'); // Ajoute une classe CSS au bouton
-    AfficherOption.appendChild(option_btn); // Ajoute le bouton à l'élément options
-});
+// Initialise l'index de la question courante à 0
+let currentQuestionIndex = 0
+
+
+function loadQuestion(currentQuestion){
+    // Récupère le texte de la première question du quiz
+    const question1 = quiz_Frida.questions[currentQuestion].text
+    // Affiche la première question dans l'élément sélectionné
+    AfficherQuestions.innerText = question1
+    // Efface les options précédentes avant d'afficher les nouvelles
+    AfficherOption.innerHTML = ""
+    // Pour chaque option de la première question, crée un bouton et l'ajoute à la page
+    quiz_Frida.questions[currentQuestion].options.forEach(elem => {
+        const option_btn = document.createElement('button'); // Crée un bouton
+        option_btn.innerText = elem; // Définit le texte du bouton
+        option_btn.classList.add('answer'); // Ajoute une classe CSS au bouton
+        AfficherOption.appendChild(option_btn); // Ajoute le bouton à l'élément options
+    });
+}
+
+loadQuestion(currentQuestionIndex)
+
+suivant.addEventListener('click', () => {
+    // Incrémente l'index de la question courante
+    currentQuestionIndex += 1
+    // Vérifie s'il reste des questions à afficher
+    if (currentQuestionIndex < quiz_Frida.questions.length) {
+        // Afficher la question suivante
+        loadQuestion(currentQuestionIndex)
+    } else {
+        // Si plus de questions, indiquer la fin du quiz
+        AfficherQuestions.innerText = 'Bravo quiz fini';
+        AfficherOption.innerHTML = ''; // Effacer les options
+        suivant.style.display ='none'; // Cacher le bouton Suivant
+    }
+})
+
+/*suivant.addEventListener('click', toTheNextPage)
+
+function toTheNextPage(){
+    currentQuestionIndex ++
+    if(currentQuestionIndex < quiz_Frida.questions.lenght){
+        loadQuestion(currentQuestionIndex)
+    } else {
+        AfficherQuestions.innerText = 'Bravo vous avez fini le quizz'
+        AfficherOption.innerHTML = ''
+        suivant.style.display='none'
+    }
+}
+
+*/
