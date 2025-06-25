8162bbc303a6c014571224f8508313f90bfbba87
Merwann_tech <merwannheret@gmail.com>
2025-06-25 09:50:01 +0200

    test

--- questions.js
+++ questions.js
@@ -52,3 +52,57 @@ export const quiz_Frida = {
         },
     ]
 };
+export const quiz_moi = {
+    questions: [
+        {
+            text: "Frida Kahlo est reconnue pour ses œuvres dans quel domaine artistique ?",
+            options: [
+                "La peinture, notamment ses autoportraits emblématiques.",
+                "La sculpture sur bois inspirée du folklore mexicain.",
+                "La photographie de paysages urbains.",
+                "La création de mosaïques en céramique."
+            ],
+            correct_answer: "La peinture, notamment ses autoportraits emblématiques."
+        },
+        {
+            text: "En plus de son travail d'artiste, dans quelle autre activité Frida Kahlo s'est-elle impliquée ?",
+            options: [
+                "Elle a dirigé un orchestre symphonique.",
+                "Elle a conçu des costumes pour le théâtre.",
+                "Elle a écrit des romans policiers.",
+                "Elle a enseigné l'art dans des écoles mexicaines.",
+            ],
+            correct_answer: "Elle a enseigné l'art dans des écoles mexicaines."
+        },
+        {
+            text: "Quel événement a profondément marqué la vie de Frida Kahlo et influencé son œuvre artistique ?",
+            options: [
+                "Elle a remporté une médaille olympique en peinture.",
+                "Elle a survécu à un grave accident de bus dans sa jeunesse",
+                "Elle a été exilée du Mexique pour raisons politiques.",
+                "Elle a grandi dans une famille de musiciens célèbres.",
+            ],
+            correct_answer: "Elle a survécu à un grave accident de bus dans sa jeunesse"
+        },
+        {
+            text: "Quel célèbre artiste Frida Kahlo a-t-elle épousé (deux fois) au cours de sa vie ?",
+            options: [
+                "Pablo Picasso",
+                "Salvador Dalí ",
+                "Diego Rivera",
+                "Henri Matisse",
+            ],
+            correct_answer: "Diego Rivera"
+        },
+        {
+            text: "Dans quelle ville se trouve la maison bleue (Casa Azul), aujourd'hui musée consacré à Frida Kahlo ?",
+            options: [
+                "Buenos Aires",
+                "Mexico",
+                "Madrid",
+                "Lima",
+            ],
+            correct_answer: "Mexico"
+        },
+    ]
+};
