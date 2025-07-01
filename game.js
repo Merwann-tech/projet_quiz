5c17dd4e9e055547bc6c3528516ce03fd2f875fb
Merwann <merwannheret@gmail.com>
2025-07-01 15:49:41 +0200

    j'ai réparé un dernier truc promi juré craché

--- game.js
+++ game.js
@@ -286,7 +286,7 @@ document.addEventListener("keydown",(e) =>{
 
 // touche F2 change le scoreboard
 document.addEventListener("keydown",(e) =>{
-    if(scoreBoard.style.display ='inline-block'){
+    if(scoreBoard.style.display =='inline-block'){
         if(e.key === "F2" ){
             if (toggleF2 == true){
                 toggleF2 = false
